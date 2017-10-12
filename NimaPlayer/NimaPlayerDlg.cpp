b47d05c4634b2ccf4657311e354bf2ff5e4ad834
jdqiu <jdqiu@sina.cn>
2017-10-13 02:11:03 +0800

    xvf

--- NimaPlayer/NimaPlayerDlg.cpp
+++ NimaPlayer/NimaPlayerDlg.cpp
@@ -15,6 +15,7 @@ extern "C"
 #include "libavformat/avformat.h"
 #include "libswscale/swscale.h"
 #include "libavutil/imgutils.h"
+#include "libswresample/swresample.h"
 #include "SDL2/SDL.h"
 };
 #else
@@ -27,6 +28,7 @@ extern "C"
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>
 #include <libavutil/imgutils.h>
+#include <libswresample/swresample.h>
 #include <SDL2/SDL.h>
 #ifdef __cplusplus
 };
@@ -185,6 +187,9 @@ HCURSOR CNimaPlayerDlg::OnQueryDragIcon()
 }
 
 //============================================================
+
+//#define VIDEO_FMT PIX_FMT_BGR24 
+#define NVIDEO_PIX_FMT AV_PIX_FMT_YUV420P 
 #define SDL_AUDIO_BUFFER_SIZE 1024
 #define MAX_AUDIO_FRAME_SIZE 192000 // 1 second of 48khz 32bit audio 
 //Refresh Event
@@ -194,6 +199,13 @@ HCURSOR CNimaPlayerDlg::OnQueryDragIcon()
 
 int thread_exit=0;
 int thread_pause=0;
+int flag=100;//标识 播放，暂停，退出 0退出，1标识，2暂停
+
+int fileDuration=0;//视频的长度  单位秒
+//快进的参数
+bool isSeek=false;//是否在快进
+int global_seek_index=0;//文件索引 快进
+double global_seek_pos=0;//快进的地方
 
 //Buffer:  
 //|-----------|-------------|  
@@ -254,6 +266,7 @@ typedef struct AudioItem{
 	AudioItem *next;
 	SDL_AudioSpec *wanted_spec;
 }AudioItem;
+
 //存储音频的队列
 typedef struct  {
 	AudioItem *firstItem;//队列头
@@ -266,7 +279,11 @@ typedef struct  {
 	SDL_cond *audioCond; //唤醒线程
 } AudioQueue;
 
-
+typedef struct {
+	int length;
+	char **urls;
+	int *times; 
+}VideoState;
 
 VideoQueue *videoQueue = NULL; //视频队列
 AudioQueue *audioQueue = NULL; //音频队列
@@ -629,12 +646,12 @@ void audio_callback(void *userdata, Uint8 *stream, int len) {
 #endif
 }
 
-int ffmpegPlay(LPVOID lpParam)
+int ffmpegPlay1(LPVOID lpParam, void *arg)
 {
 	CString timelong;
 	float /*framerate_temp,*/ timelong_temp/*, bitrate_temp*/;
 	AVFormatContext	*pFormatCtx;
-	int				i, video_stream, audio_stream;
+	int				i, video_index, audio_index;
 	AVCodecContext	*pCodecCtx, *pCodecCtx_au;
 	AVCodec			*pCodec, *pCodec_au;
 	AVFrame	*pFrame,*pFrameYUV;
@@ -654,6 +671,10 @@ int ffmpegPlay(LPVOID lpParam)
 
 	struct SwsContext *img_convert_ctx;
 
+	VideoState *vs = (VideoState *)arg;
+	int length = vs->length;
+	double currentAllFilePts = 0;
+
 	char tempMsgBuf[255] = {0};
 	CString str_msg;
 	//char filepath[]="bigbuckbunny_480x272.h265";
@@ -670,11 +691,15 @@ int ffmpegPlay(LPVOID lpParam)
 	//sprintf_s(tempMsgBuf, "file path = %s", filePath);
 	//str_msg = tempMsgBuf;
 	//AfxMessageBox(str_msg);
-	av_register_all();
-	avformat_network_init();
+	av_register_all();//注册所有解码器
+	avformat_network_init();//初始化流媒体格式
+	for (int j = 0; j < length; j++){
+
+	double currentFilePts = 0;
+	char *url = vs->urls[j];
 	pFormatCtx = avformat_alloc_context();
 
-	if(avformat_open_input(&pFormatCtx,filePath,NULL,NULL)!=0){
+	if(avformat_open_input(&pFormatCtx,url,NULL,NULL)!=0){
 		AfxMessageBox(L"Couldn't open input stream.\n");
 		return -1;
 	}
@@ -684,23 +709,24 @@ int ffmpegPlay(LPVOID lpParam)
 		AfxMessageBox(L"Couldn't find stream information.\n");
 		return -1;
 	}
-	video_stream=audio_stream=-1;
+	video_index=audio_index=-1;
 	for(i=0; i<pFormatCtx->nb_streams; i++) {
 		if(pFormatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO){
-			video_stream=i;
+			video_index=i;
 		}
 		if(pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_AUDIO){
-			audio_stream = i;
+			audio_index = i;
 		}
 	}
-		if(audio_stream == -1 && video_stream == -1){
+		if(audio_index == -1 && video_index == -1){
 			AfxMessageBox(_T("Didn't find a video/audio stream."));
 			return -1;
 		}
 
 		//视频解码参数
-		if(video_stream != -1){
-			pCodecCtx=pFormatCtx->streams[video_stream]->codec;
+		if(video_index != -1){
+			//视频解码器上下文
+			pCodecCtx=pFormatCtx->streams[video_index]->codec;
 			pCodec=avcodec_find_decoder(pCodecCtx->codec_id);
 			if(pCodec==NULL){
 				AfxMessageBox(L"Codec not found.\n");
@@ -712,9 +738,10 @@ int ffmpegPlay(LPVOID lpParam)
 			}
 		}
 		//音频解码参数
-		if(audio_stream != -1){
-			pCodecCtx_au = pFormatCtx->streams[audio_stream]->codec;
+		if(audio_index != -1){
+			pCodecCtx_au = pFormatCtx->streams[audio_index]->codec;
 
+#if 0
 			// Set audio settings from codec info
 			wanted_spec.freq = pCodecCtx_au->sample_rate;
 			wanted_spec.format = AUDIO_S16SYS;
@@ -728,6 +755,7 @@ int ffmpegPlay(LPVOID lpParam)
 				fprintf(stderr, "SDL_OpenAudio: %s\n", SDL_GetError());
 				return -1;
 			}
+#endif
 
 			pCodec_au = avcodec_find_decoder(pCodecCtx_au->codec_id);
 			if(pCodec_au == NULL){
@@ -739,41 +767,70 @@ int ffmpegPlay(LPVOID lpParam)
 				AfxMessageBox(_T("Could not open audio codec."));
 				return -1;
 			}
-			packet_queue_init(&audioq);
-			SDL_PauseAudio(0);
 		}
-		//duration是以微秒为单位
-		timelong_temp = pFormatCtx->duration / 1000000;
-		int tns, thh, tmm, tss;
-		tns = pFormatCtx->duration /1000000;
-		thh = tns /3600;
-		tmm = (tns % 3600)/ 60;
-		tss = (tns % 60);
-		timelong.Format(_T("%02d:%02d:%02d"), thh, tmm, tss);
-		//timelong.Format(_T("pFormatCtx->duration = %lld,  ::%02d:%02d:%02d"),pFormatCtx->duration, thh, tmm, tss);
-		//AfxMessageBox(timelong);
-		if(tns < 0){
-			timelong = _T("--:--:--");
-		}
-		dlg->m_sta_duration.SetWindowText(timelong);
+		////duration是以微秒为单位
+		//timelong_temp = pFormatCtx->duration / 1000000;
+		//int tns, thh, tmm, tss;
+		//tns = pFormatCtx->duration /1000000;
+		//thh = tns /3600;
+		//tmm = (tns % 3600)/ 60;
+		//tss = (tns % 60);
+		//timelong.Format(_T("%02d:%02d:%02d"), thh, tmm, tss);
+		////timelong.Format(_T("pFormatCtx->duration = %lld,  ::%02d:%02d:%02d"),pFormatCtx->duration, thh, tmm, tss);
+		////AfxMessageBox(timelong);
+		//if(tns < 0){
+		//	timelong = _T("--:--:--");
+		//}
+		//dlg->m_sta_duration.SetWindowText(timelong);
 
 		pFrame=av_frame_alloc();//保存原始的帧
 		pFrameYUV=av_frame_alloc();//保存转换后的帧
+		int frameFinished=0;//是否凑成一帧数据
+		int result=0;//标识一个视频是否解码完毕
+		int audioLength=0;//音频数组的长度
+		int videoLength=0;//视频数组的长度
 
-		out_buffer=(unsigned char *)av_malloc(av_image_get_buffer_size(AV_PIX_FMT_YUV420P,  pCodecCtx->width, pCodecCtx->height,1));//放置原始数据
-		av_image_fill_arrays(pFrameYUV->data, pFrameYUV->linesize,out_buffer,
-			AV_PIX_FMT_YUV420P,pCodecCtx->width, pCodecCtx->height,1);//把帧和新申请的内存结合
+		//把视频帧转化为数组参数
+		struct SwsContext *img_convert_ctx;
+		if( video_index != -1){
+			
+			videoLength = av_image_get_buffer_size(NVIDEO_PIX_FMT,  pCodecCtx->width, pCodecCtx->height,1);
+			img_convert_ctx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt, 
+				pCodecCtx->width, pCodecCtx->height, NVIDEO_PIX_FMT, SWS_BICUBIC, NULL, NULL, NULL); 
+		}
 
-		//Output Info-----------------------------
-		printf("---------------- File Information ---------------\n");
-		//av_dump_format(pFormatCtx,0,filePath,0);
+
+		//out_buffer=(unsigned char *)av_malloc(av_image_get_buffer_size(AV_PIX_FMT_YUV420P,  pCodecCtx->width, pCodecCtx->height,1));//放置原始数据
+		//av_image_fill_arrays(pFrameYUV->data, pFrameYUV->linesize,out_buffer,
+		//	AV_PIX_FMT_YUV420P,pCodecCtx->width, pCodecCtx->height,1);//把帧和新申请的内存结合
+
+		////Output Info-----------------------------
+		//printf("---------------- File Information ---------------\n");
+		////av_dump_format(pFormatCtx,0,filePath,0);
+		//
+		//printf("-------------------------------------------------\n");
+
+		//img_convert_ctx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt, 
+		//	pCodecCtx->width, pCodecCtx->height, AV_PIX_FMT_YUV420P, SWS_BICUBIC, NULL, NULL, NULL); 
+		//把音频帧转化为数组的参数
+		//uint64_t out_channel_layout=AV_CH_LAYOUT_STEREO; 
+		AVSampleFormat out_sample_fmt=AV_SAMPLE_FMT_S16;
+		int out_sample_rate=44100;
+		int64_t in_channel_layout=av_get_channel_layout_nb_channels(pCodecCtx_au->channels);
+
+		int out_channels=av_get_channel_layout_nb_channels(pCodecCtx_au->channels);
+		int out_nb_samples=1024;
+		audioLength=av_samples_get_buffer_size(NULL,out_channels ,out_nb_samples,out_sample_fmt, 1);
+
+		struct SwrContext *au_convert_ctx; 
+		au_convert_ctx = swr_alloc(); 
+		au_convert_ctx=swr_alloc_set_opts(au_convert_ctx,pCodecCtx_au->channels, out_sample_fmt, out_sample_rate, 
+			in_channel_layout,pCodecCtx_au->sample_fmt , pCodecCtx_au->sample_rate,0, NULL); 
+		swr_init(au_convert_ctx); 
+		int sample=SDL_AUDIO_BUFFER_SIZE;
+
 		
-		printf("-------------------------------------------------\n");
-
-		img_convert_ctx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt, 
-			pCodecCtx->width, pCodecCtx->height, AV_PIX_FMT_YUV420P, SWS_BICUBIC, NULL, NULL, NULL); 
-
-
+		}
 		if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER)) {  
 			sprintf_s( tempMsgBuf, "Could not initialize SDL - %s\n", SDL_GetError()); 
 			str_msg = tempMsgBuf;
@@ -849,10 +906,32 @@ int ffmpegPlay(LPVOID lpParam)
 					if(av_read_frame(pFormatCtx, packet)<0)
 						thread_exit=1;
 
-					if(packet->stream_index == video_stream || packet->stream_index == audio_stream)
+					if(packet->stream_index == video_index || packet->stream_index == audio_index)
 						break;
 				}
-				if( packet->stream_index == video_stream ){
+				if(isSeek) //要快进
+				{
+					//快进
+					if( j == global_seek_index){
+						int seekFlag = avformat_seek_file(pFormatCtx,
+							-1, 
+							(global_seek_pos - 10)* AV_TIME_BASE, 
+							global_seek_pos *AV_TIME_BASE,
+							(global_seek_pos + 10)*AV_TIME_BASE,
+							AVSEEK_FLAG_ANY);
+						if(seekFlag >= 0){
+							currentAllFilePts = 0;
+							for (int k= 0; k < j; k++)
+								currentAllFilePts += vs->times[k];
+						}
+						isSeek = false;
+					}else{
+						j = global_seek_index - 1;
+						break;
+					}
+				}
+
+				if( packet->stream_index == video_index ){
 					ret = avcodec_decode_video2(pCodecCtx, pFrame, &got_picture, packet);
 					if(ret < 0){
 						AfxMessageBox(L"Decode Error.\n");
@@ -869,7 +948,7 @@ int ffmpegPlay(LPVOID lpParam)
 						//SDL End-----------------------
 					}
 					av_free_packet(packet);
-				}else if( packet->stream_index == audio_stream ){
+				}else if( packet->stream_index == audio_index ){
 					packet_queue_put(&audioq, packet);
 				} else {
 					av_free_packet(packet);
@@ -899,7 +978,312 @@ int ffmpegPlay(LPVOID lpParam)
 
 		return 0;
 }
+
+//获取视频的总长度
+void InitAllTime(VideoState *vs)
+{
+	fileDuration=0;
+	int length=vs->length;
+	av_register_all();  //注册所有解码器
+	avformat_network_init();  //初始化流媒体格式   
+	for (int j = 0; j < length; j++)
+	{
+		char* url=vs->urls[j];
+		AVFormatContext *pFormatCtx = avformat_alloc_context(); 
+		//打开文件
+		if(avformat_open_input(&pFormatCtx,url,NULL,NULL)!=0)
+		{ 
+			//strcpy(ErrorMsg,"无法打开网络流");
+			return;
+		}
+		if(av_find_stream_info(pFormatCtx)<0) 
+		{ 
+			//strcpy(ErrorMsg,"无法获取流信息"); 
+			return; 
+		}
+		//保存每个文件的播放长度
+		vs->times[j]=pFormatCtx->duration/1000000;
+		//获取此视频的总时间 微妙转化为妙
+		fileDuration+= vs->times[j];
+
+		avformat_close_input(&pFormatCtx);
+	}
+	avformat_network_deinit();
+}
 //============================================================
+
+int ffmpegPlay(LPVOID lpParam)
+{
+	CString timelong;
+	float /*framerate_temp,*/ timelong_temp/*, bitrate_temp*/;
+	AVFormatContext	*pFormatCtx;
+	int				i, video_index, audio_index;
+	AVCodecContext	*pCodecCtx, *pCodecCtx_au;
+	AVCodec			*pCodec, *pCodec_au;
+	AVFrame	*pFrame,*pFrameYUV;
+	unsigned char *out_buffer;
+	AVPacket *packet;
+	int ret, got_picture;
+
+	//------------SDL----------------
+	int screen_w,screen_h;
+	SDL_Window *screen; 
+	SDL_Renderer* sdlRenderer;
+	SDL_Texture* sdlTexture;
+	SDL_Rect sdlRect;
+	SDL_Thread *video_tid;
+	SDL_Event event;
+	SDL_AudioSpec   wanted_spec, spec;
+
+	struct SwsContext *img_convert_ctx;
+
+	char tempMsgBuf[255] = {0};
+	CString str_msg;
+	//char filepath[]="bigbuckbunny_480x272.h265";
+	//char filepath[]="Titanic.ts";
+	//文件路径如下
+	CNimaPlayerDlg *dlg = (CNimaPlayerDlg *)lpParam;
+	wchar_t wFilePath[250] = {0};
+	GetWindowText(dlg->m_edit_url, (LPTSTR)wFilePath, 250);
+
+	int bufSize=WideCharToMultiByte(CP_ACP,NULL,wFilePath,-1,NULL,0,NULL,FALSE);
+	char *filePath=new char[bufSize];
+	WideCharToMultiByte(CP_ACP,NULL,wFilePath,-1,filePath,bufSize,NULL,FALSE);
+
+	//sprintf_s(tempMsgBuf, "file path = %s", filePath);
+	//str_msg = tempMsgBuf;
+	//AfxMessageBox(str_msg);
+	av_register_all();//注册所有解码器
+	avformat_network_init();//初始化流媒体格式
+	pFormatCtx = avformat_alloc_context();
+
+	if(avformat_open_input(&pFormatCtx,filePath,NULL,NULL)!=0){
+		AfxMessageBox(L"Couldn't open input stream.\n");
+		return -1;
+	}
+	delete[] filePath;
+
+	if(avformat_find_stream_info(pFormatCtx,NULL)<0){
+		AfxMessageBox(L"Couldn't find stream information.\n");
+		return -1;
+	}
+	video_index=audio_index=-1;
+	for(i=0; i<pFormatCtx->nb_streams; i++) {
+		if(pFormatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO){
+			video_index=i;
+		}
+		if(pFormatCtx->streams[i]->codec->codec_type == AVMEDIA_TYPE_AUDIO){
+			audio_index = i;
+		}
+	}
+	if(audio_index == -1 && video_index == -1){
+		AfxMessageBox(_T("Didn't find a video/audio stream."));
+		return -1;
+	}
+
+	//视频解码参数
+	if(video_index != -1){
+		//视频解码器上下文
+		pCodecCtx=pFormatCtx->streams[video_index]->codec;
+		pCodec=avcodec_find_decoder(pCodecCtx->codec_id);
+		if(pCodec==NULL){
+			AfxMessageBox(L"Codec not found.\n");
+			return -1;
+		}
+		if(avcodec_open2(pCodecCtx, pCodec,NULL)<0){
+			AfxMessageBox(L"Could not open codec.\n");
+			return -1;
+		}
+	}
+	//音频解码参数
+	if(audio_index != -1){
+		pCodecCtx_au = pFormatCtx->streams[audio_index]->codec;
+
+		// Set audio settings from codec info
+		wanted_spec.freq = pCodecCtx_au->sample_rate;
+		wanted_spec.format = AUDIO_S16SYS;
+		wanted_spec.channels = pCodecCtx_au->channels;
+		wanted_spec.silence = 0;
+		wanted_spec.samples = SDL_AUDIO_BUFFER_SIZE; //pCodecCtx_au->frame_size; 
+		wanted_spec.callback = audio_callback;
+		wanted_spec.userdata = pCodecCtx_au;
+
+		if(SDL_OpenAudio(&wanted_spec, &spec) < 0) {
+			fprintf(stderr, "SDL_OpenAudio: %s\n", SDL_GetError());
+			return -1;
+		}
+
+		pCodec_au = avcodec_find_decoder(pCodecCtx_au->codec_id);
+		if(pCodec_au == NULL){
+			AfxMessageBox(_T("Audio codec not found."));
+			return -1;
+		}
+
+		if(avcodec_open2(pCodecCtx_au, pCodec_au, NULL) < 0 ){
+			AfxMessageBox(_T("Could not open audio codec."));
+			return -1;
+		}
+		packet_queue_init(&audioq);
+		SDL_PauseAudio(0);
+	}
+	//duration是以微秒为单位
+	timelong_temp = pFormatCtx->duration / 1000000;
+	int tns, thh, tmm, tss;
+	tns = pFormatCtx->duration /1000000;
+	thh = tns /3600;
+	tmm = (tns % 3600)/ 60;
+	tss = (tns % 60);
+	timelong.Format(_T("%02d:%02d:%02d"), thh, tmm, tss);
+	//timelong.Format(_T("pFormatCtx->duration = %lld,  ::%02d:%02d:%02d"),pFormatCtx->duration, thh, tmm, tss);
+	//AfxMessageBox(timelong);
+	if(tns < 0){
+		timelong = _T("--:--:--");
+	}
+	dlg->m_sta_duration.SetWindowText(timelong);
+
+	pFrame=av_frame_alloc();//保存原始的帧
+	pFrameYUV=av_frame_alloc();//保存转换后的帧
+
+	out_buffer=(unsigned char *)av_malloc(av_image_get_buffer_size(AV_PIX_FMT_YUV420P,  pCodecCtx->width, pCodecCtx->height,1));//放置原始数据
+	av_image_fill_arrays(pFrameYUV->data, pFrameYUV->linesize,out_buffer,
+		AV_PIX_FMT_YUV420P,pCodecCtx->width, pCodecCtx->height,1);//把帧和新申请的内存结合
+
+	//Output Info-----------------------------
+	printf("---------------- File Information ---------------\n");
+	//av_dump_format(pFormatCtx,0,filePath,0);
+
+	printf("-------------------------------------------------\n");
+
+	img_convert_ctx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt, 
+		pCodecCtx->width, pCodecCtx->height, AV_PIX_FMT_YUV420P, SWS_BICUBIC, NULL, NULL, NULL); 
+
+
+	if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER)) {  
+		sprintf_s( tempMsgBuf, "Could not initialize SDL - %s\n", SDL_GetError()); 
+		str_msg = tempMsgBuf;
+		AfxMessageBox(str_msg);
+		return -1;
+	} 
+
+
+	//SDL 2.0 Support for multiple windows
+	screen_w = pCodecCtx->width;
+	screen_h = pCodecCtx->height;
+	//显示在弹出窗口
+	//screen = SDL_CreateWindow("Simplest ffmpeg player's Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+	//	screen_w, screen_h,SDL_WINDOW_OPENGL);
+	//显示在MFC控件上
+	screen = SDL_CreateWindowFrom(dlg->GetDlgItem(IDC_PIC)->GetSafeHwnd());
+	if(!screen) {  
+		sprintf_s(tempMsgBuf, "SDL: could not create window - exiting:%s\n",SDL_GetError()); 
+		str_msg = tempMsgBuf;
+		AfxMessageBox(str_msg);
+		return -1;
+	}
+	sdlRenderer = SDL_CreateRenderer(screen, -1, 0);  
+	//IYUV: Y + U + V  (3 planes)
+	//YV12: Y + V + U  (3 planes)
+	sdlTexture = SDL_CreateTexture(sdlRenderer, SDL_PIXELFORMAT_IYUV, SDL_TEXTUREACCESS_STREAMING,pCodecCtx->width,pCodecCtx->height);  
+
+	sdlRect.x=0;
+	sdlRect.y=0;
+	sdlRect.w=screen_w;
+	sdlRect.h=screen_h;
+
+	packet=(AVPacket *)av_malloc(sizeof(AVPacket));
+	//-------------------------------
+	//av_init_packet(packet); 
+	//Out Audio Param  
+	//uint64_t out_channel_layout=AV_CH_LAYOUT_STEREO;  
+	//nb_samples: AAC-1024 MP3-1152  
+	//int out_nb_samples=pCodecCtx_au->frame_size;  
+	//AVSampleFormat out_sample_fmt=AV_SAMPLE_FMT_S16;  
+	//int out_sample_rate=44100;  
+	//int out_channels=av_get_channel_layout_nb_channels(out_channel_layout);  
+	//Out Buffer Size  
+	//int out_buffer_size=av_samples_get_buffer_size(NULL,out_channels ,out_nb_samples,out_sample_fmt, 1);  
+
+	//out_buffer=(uint8_t *)av_malloc(MAX_AUDIO_FRAME_SIZE*2);  
+	//pFrame=av_frame_alloc();  
+
+	//SDL_AudioSpec  
+	//wanted_spec.freq = out_sample_rate;   
+	//wanted_spec.format = AUDIO_S16SYS;   
+	//wanted_spec.channels = out_channels;   
+	//wanted_spec.silence = 0;   
+	//wanted_spec.samples = out_nb_samples;   
+	//wanted_spec.callback = fill_audio;   
+	//wanted_spec.userdata = pCodecCtx;   
+
+	//if (SDL_OpenAudio(&wanted_spec, NULL)<0){   
+	//	printf("can't open audio.\n");   
+	//	return -1;   
+	//}   
+	//---------------------------------
+
+	video_tid = SDL_CreateThread(sfp_refresh_thread,NULL,NULL);
+	//------------SDL End------------
+	//Event Loop
+
+	for (;;) {
+		//Wait
+		SDL_WaitEvent(&event);
+		if(event.type==SFM_REFRESH_EVENT){
+			while(1){
+				if(av_read_frame(pFormatCtx, packet)<0)
+					thread_exit=1;
+
+				if(packet->stream_index == video_index || packet->stream_index == audio_index)
+					break;
+			}
+			if( packet->stream_index == video_index ){
+				ret = avcodec_decode_video2(pCodecCtx, pFrame, &got_picture, packet);
+				if(ret < 0){
+					AfxMessageBox(L"Decode Error.\n");
+					return -1;
+				}
+				if(got_picture){
+					sws_scale(img_convert_ctx, (const unsigned char* const*)pFrame->data, pFrame->linesize, 0, pCodecCtx->height, pFrameYUV->data, pFrameYUV->linesize);
+					//SDL---------------------------
+					SDL_UpdateTexture( sdlTexture, NULL, pFrameYUV->data[0], pFrameYUV->linesize[0] );  
+					SDL_RenderClear( sdlRenderer );  
+					//SDL_RenderCopy( sdlRenderer, sdlTexture, &sdlRect, &sdlRect );  
+					SDL_RenderCopy( sdlRenderer, sdlTexture, NULL, NULL);  
+					SDL_RenderPresent( sdlRenderer );  
+					//SDL End-----------------------
+				}
+				av_free_packet(packet);
+			}else if( packet->stream_index == audio_index ){
+				packet_queue_put(&audioq, packet);
+			} else {
+				av_free_packet(packet);
+			}
+			av_free_packet(packet);
+		}else if(event.type==SDL_KEYDOWN){
+			//Pause
+			if(event.key.keysym.sym==SDLK_SPACE)
+				thread_pause=!thread_pause;
+		}else if(event.type==SDL_QUIT){
+			thread_exit=1;
+		}else if(event.type==SFM_BREAK_EVENT){
+			break;
+		}
+
+	}
+
+	sws_freeContext(img_convert_ctx);
+
+	SDL_Quit();
+	dlg->GetDlgItem(IDC_PIC)->ShowWindow(SW_SHOWNORMAL);
+	//--------------
+	av_frame_free(&pFrameYUV);
+	av_frame_free(&pFrame);
+	avcodec_close(pCodecCtx);
+	avformat_close_input(&pFormatCtx);
+
+	return 0;
+}
+
 //播放的线程
 UINT Thread_Play(LPVOID lpParam){
 	CNimaPlayerDlg *dlg = (CNimaPlayerDlg *)lpParam;
